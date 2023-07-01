9e825474b9033ff42581fa0ec9042586c013e88e
Rocky <smrockypk@gmail.com>
2023-07-01 13:10:15 +0600

    fixed coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -43,6 +43,7 @@ module.exports = {
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
     "json",
+    "json-summary",
   //   "text",
   //   "lcov",
   //   "clover"
