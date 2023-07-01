40fb4c82509a606562aacbe5d27fe16e2fcbe69b
Rocky <smrockypk@gmail.com>
2023-07-01 10:36:27 +0600

    Tests functional

--- src/components/input/InputBase.test.tsx
+++ src/components/input/InputBase.test.tsx
@@ -1,34 +1,28 @@
-import {render} from "@testing-library/react";
+import {cleanup, render} from "@testing-library/react";
 import {Input} from "components/input/InputBase";
 
-describe('render with default parameters', function () {
-    render(<Input name="test"/>);
+afterEach(cleanup);
+
+const setup = () => {
+    render(<Input name="test" type="email" placeholder="Email Address"/>);
+}
+
+describe('<Input />', function () {
+    beforeEach(setup);
 
     it('should render an input', function () {
-        expect(document.getElementsByTagName("input").length).toBe(1);
+        expect(document.querySelectorAll("input").length).toBe(1);
     });
 
     it('should have the name "test"', function () {
-        expect(document.getElementsByTagName("input")[0].name).toBe("test");
+        expect(document.querySelector("input")?.name).toBe("test");
     });
-
-    it('should have the type "text"', function () {
-        expect(document.getElementsByTagName("input")[0].type).toBe("text");
-    });
-
-    it('should not render a label', function() {
-        expect(document.getElementsByTagName("label")).toBeFalsy();
-    });
-});
-
-describe('render with custom parameters', function () {
-    render(<Input name="test" type="email" placeholder="Email Address"/>);
 
     it('should render input with type "email"', function () {
-        expect(document.getElementsByTagName('input')[0].type).toBe('email');
+        expect(document.querySelector('input')?.type).toBe('email');
     });
 
     it('should render input with placeholder "Email Address"', function () {
-        expect(document.getElementsByTagName('input')[0].type).toBe('Email Address');
+        expect(document.querySelector('input')?.placeholder).toBe('Email Address');
     });
 });
--- src/components/input/TextBox.test.tsx
+++ src/components/input/TextBox.test.tsx
@@ -1,29 +1,56 @@
-import {render} from "@testing-library/react";
+import {cleanup, fireEvent, render, RenderResult} from "@testing-library/react";
 import TextBox from "components/input/TextBox";
 
-describe('TextBox', function () {
-    render(<TextBox name="email" type="email" placeholder="Email Address" label="Email Address" /> )
+afterEach(cleanup);
 
-    it('should render an input with the name "email"', function() {
-        expect(document.getElementsByTagName('input')[0].name).toBe('email');
+const onChange = jest.fn();
+const onFocus = jest.fn();
+let textBox: RenderResult;
+let input: HTMLInputElement;
+
+async function setup() {
+    textBox = render(<TextBox name="email"
+                              type="email"
+                              placeholder="Email Address"
+                              label="Email Address"
+                              onFocus={onFocus}
+                              onChange={onChange}/>)
+    input = await textBox.findByPlaceholderText('Email Address') as HTMLInputElement;
+}
+
+describe('<TextBox />', function () {
+    beforeEach(setup);
+
+    it('should render an input with the name "email"', function () {
+        expect(document.querySelector('input')?.name).toBe('email');
     });
 
-    it('should render an input with the type "email"', function() {
-        expect(document.getElementsByTagName('input')[0].type).toBe('email');
+    it('should render an input with the type "email"', function () {
+        expect(document.querySelector('input')?.type).toBe('email');
     });
 
-    it('should render an input with placeholder "Email Address"', function() {
-        expect(document.getElementsByTagName('input')[0].placeholder).toBe('Email Address');
+    it('should render an input with placeholder "Email Address"', function () {
+        expect(document.querySelector('input')?.placeholder).toBe('Email Address');
     });
 
-    it('should render and associate input with label "Email Address"', function() {
-        const label = document.getElementsByTagName('label')[0];
-        const input = document.getElementsByTagName('input')[0];
+    it('should render and associate input with label "Email Address"', function () {
+        const label = document.querySelector('label');
+        const input = document.querySelector('input');
 
         expect(label).toBeTruthy();
 
-        expect(label.htmlFor).toBe(input.id);
+        expect(label?.htmlFor).toBe(input?.id);
+        expect(label?.innerHTML).toBe('Email Address');
+    });
 
-        expect(label.innerText).toBe('Email Address');
+    it('should emit onChange event on input', async function () {
+        fireEvent.change(input, {target: {value: 'xxx'}});
+        expect(input.value).toBe('xxx');
+        expect(onChange).toBeCalled();
+    });
+
+    it('should emit onFocus event on focus', async function () {
+        fireEvent.focus(input);
+        expect(onFocus).toBeCalled();
     });
 });
