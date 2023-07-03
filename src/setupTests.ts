984698fef76f7af38538310430c87a5eadbd0a4b
Rocky <smrockypk@gmail.com>
2023-07-03 19:58:52 +0600

    removed unused code

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,8 @@
 import matchers from "@testing-library/jest-dom/matchers";
 import {cleanup} from "@testing-library/react";
 import {afterEach, expect} from "vitest";
-import MockServer from "../plugins/mock-server/server";
 
 expect.extend(matchers);
-
-export let server: MockServer;
 
 afterEach(() => {
     cleanup();
