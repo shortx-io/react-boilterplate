a4d17fe28cbe8e0546225d73bfd03937d2ea8e15
Rocky <smrockypk@gmail.com>
2023-07-03 10:56:41 +0600

    Regression test for TextBox error

--- src/components/input/TextBox.test.tsx
+++ src/components/input/TextBox.test.tsx
@@ -1,6 +1,7 @@
 import {cleanup, fireEvent, render, RenderResult} from "@testing-library/react";
 import TextBox from "components/input/TextBox";
 import {vi} from "vitest";
+import "@/i18n";
 
 afterEach(cleanup);
 
@@ -9,20 +10,22 @@ const onFocus = vi.fn();
 let textBox: RenderResult;
 let input: HTMLInputElement;
 
-async function setup() {
+async function setup(data: { error?: boolean, message?: string }) {
     textBox = render(<TextBox name="email"
                               type="email"
                               placeholder="Email Address"
                               label="Email Address"
                               onFocus={onFocus}
                               required
+                              error={data.error}
+                              message={data.message}
                               defaultValue="smrockypk@gmail.com"
                               onChange={onChange}/>);
     input = await textBox.findByPlaceholderText("Email Address") as HTMLInputElement;
 }
 
 describe("<TextBox />", function() {
-    beforeEach(setup);
+    beforeEach(() => setup({error: false}));
 
     it("should render an input with the name \"email\"", function() {
         expect(document.querySelector("input")?.name).toBe("email");
@@ -67,5 +70,22 @@ describe("<TextBox />", function() {
 
     it("should render input with an id", function() {
         expect(input.id).toBeTruthy();
+    });
+
+    it("should render input with without error message",function() {
+        expect(document.getElementById(`email-error`)).toBeFalsy();
+    });
+});
+
+describe("<TextBox error />", function() {
+    const message = 'Error Message'
+    beforeEach(() => setup({error: true, message}))
+
+    it("should render input with with error message",function() {
+        expect(document.getElementById(`email-error`)).toHaveTextContent(message);
+    });
+
+    it("should render input marked with red color", function() {
+        expect(document.querySelector('input')).toHaveClass('text-red-900', 'ring-red-300', 'ring-inset', 'ring-1');
     });
 });
